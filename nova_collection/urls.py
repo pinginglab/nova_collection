0ab0335f595dea429d6dbf5004b8dffa29d64b38
yerikyu <19802021029@139.com>
2019-05-02 12:48:13 +0800

    Thu May  2 12:48:13 CST 2019

--- nova_collection/urls.py
+++ nova_collection/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^account/', include('apps.users.urls', namespace="account")),
+    # 用户操作相关URL配置
+    url(r'^account/', include('apps.users.urls')),
 ]
