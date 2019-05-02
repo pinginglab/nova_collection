5688eba3816f37dd2cd91d3a79faf8f3d6b7ba5a
yerikyu <19802021029@139.com>
2019-05-02 16:23:14 +0800

    dockerfile upgrade

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip freeze > requirement
-docker build -t nova:latest .
-docker tag nova:latest registry.pinginglab.com:5000/beckend/nova:1.0
-docker push registry.pinginglab.com:5000/beckend/nova:1.0
+docker build -t novacollection:latest .
+docker tag novacollection:latest registry.pinginglab.com:5000/beckend/novacollection:1.0
+docker push registry.pinginglab.com:5000/beckend/novacollection:1.0
